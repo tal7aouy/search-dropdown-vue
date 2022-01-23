17289213a3ca43c2b88f674f7e23627363add661
tal7aouy <talhaouycoding@gmail.com>
2022-01-23 22:41:33 +0100

    searchDrodown demo

---
+++ demo/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import SearchDropdown from 'search-dropdown-vue';
+const countries = ref([
+  { id: 1, name: 'Morocco' },
+  { id: 2, name: 'USA' },
+  { id: 3, name: 'France' },
+  { id: 4, name: 'China' },
+  { id: 5, name: 'Canada' },
+]);
+let object = reactive({ id: null, name: null });
+
+const onSelectedOption = (payload) => (object = payload);
+</script>
+
+<template>
+  <SearchDropdown
+    :class="dropdown"
+    :options="countries"
+    v-on:selected="onSelectedOption"
+  />
+</template>
